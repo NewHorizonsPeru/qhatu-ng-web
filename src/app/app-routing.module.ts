ebd067382a6fb8422e839c9463a35a97c8d9ed75
Renato De la Rosa <srdelarosab@gmail.com>
2022-12-23 22:11:55 -0500

    Template Version 1.0.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './core/layout/layout.component';
 import { LoginComponent } from './login/login.component';
+import { ProductComponent } from './product/product.component';
 import { RegisterComponent } from './register/register.component';
+import { SalesComponent } from './sales/sales.component';
 
 const routes: Routes = [
   {
-    path: 'home',
-    component: HomeComponent,
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'products',
+        pathMatch: 'full',
+      },
+      {
+        path: 'products',
+        component: ProductComponent,
+      },
+      {
+        path: 'sales',
+        component: SalesComponent,
+      },
+    ],
   },
   {
     path: 'register',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,27 @@ import { AppComponent } from './app.component';
 import { MaterialModule } from './core/material/material.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutComponent } from './core/layout/layout.component';
+import { ProductComponent } from './product/product.component';
+import { SalesComponent } from './sales/sales.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegisterComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, MaterialModule, FormsModule],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    LayoutComponent,
+    ProductComponent,
+    SalesComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    FormsModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
