e35aade3724a2c5c4adaa2a815140daf566a5414
Renato De la Rosa <srdelarosab@gmail.com>
2022-12-27 19:04:59 -0500

    Config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
 export const environment = {
   production: true,
+  apiQhatu: {
+    baseUrl: 'http://localhost:2705/api/v1/',
+    signInPath: 'security/signIn',
+    signUpPath: 'security/signUp',
+    getRoles: 'security/getRoles',
+  },
+  webStorage: {
+    tokenKey: 'QHATU_TOKEN',
+    userKey: 'QHATU_USER',
+  },
+  jwtConfig: {
+    keyValueHeader: 'Authorization',
+    keyToken: 'Bearer',
+  },
+  reCaptchaConfig: {
+    keyWebSite: '6LfDuLEjAAAAAId6A5mb4NLCsK9IooZrInPpuccV',
+    keySecret: '6LfDuLEjAAAAALfEXjh4KJvQFxjJp5hOoAtrs0Gp',
+  },
 };
